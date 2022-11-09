8c7e88f0733467ce816f1cff5c90db874f27c72b
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-11-09 16:46:25 +0300

    Example Controller and View Added

---
+++ MyAspNetCoreApp.Web/Controllers/ExampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
