7070b429a9db2bbcff66dd3335e62cd3c02607f2
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-11-09 18:48:06 +0300

    CustomLayout + NoLayout example

---
+++ MyAspNetCoreApp.Web/Controllers/Example2Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class Example2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IndexNoLayout()
+        {
+            return View();
+        }
+    }
+}
