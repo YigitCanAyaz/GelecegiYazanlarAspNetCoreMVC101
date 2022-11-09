d2df36eeded8a77a0c5493559c3fe6922026d465
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-11-09 16:25:45 +0300

    Basic Routing Structure

--- MyAspNetCoreApp.Web/Program.cs
+++ MyAspNetCoreApp.Web/Program.cs
@@ -24,4 +24,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// baseUrl/products/pencil/1 => 1 is not mandatory
+// baseUrl/home/index => default
+// baseUrl/home/privacy
+
 app.Run();
